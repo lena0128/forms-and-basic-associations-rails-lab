55e42357b37b7d9439f3ca374561c637de986617
Lena Hou <lenahou2013@gmail.com>
2021-07-27 14:40:57 -0400

    commit tree 9d11ef28f76ff590c725baaac862d27ee531ba4e

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,29 @@
 class Song < ActiveRecord::Base
   # add associations here
+
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name) #setter method
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(id)
+    self.genre = Genre.find_or_create_by(id: id)
+  end
+
+
+  def song_notes=(notes)
+    notes.each do |note|
+      if note != ""
+        self.notes.build(content: note)
+      end
+    end
+  end
+
 end
